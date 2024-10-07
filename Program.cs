8c541fea7113e0352e64ed892f8125a8e2cd225b
hiro12a <ythom87@gmail.com>
2024-10-07 15:02:27 -0400

    update

--- Program.cs
+++ Program.cs
@@ -104,7 +104,7 @@ builder.Services.AddSwaggerGen(option =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
